fcc6a26125dcc697b4eb7d55cca2474bacc1130e
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-06-17 15:38:02 +0200

    Normalize paths based on config.json dirname

--- src/options.js
+++ src/options.js
@@ -3,15 +3,32 @@ var path = require('path')
 
 var Options = function(configPath) {
 
-    var rawData       = fs.readFileSync(path.join(process.cwd(), configPath), 'utf8');
+    this.configPath = this.normalizeConfigPath(configPath)
+    this.configDirname = path.dirname(this.configPath)
+
+    var rawData       = fs.readFileSync(this.configPath, 'utf8');
     var data          = JSON.parse(rawData)
-    this.import_paths = data.import_paths
-    this.css_dir      = data.css_dir
-    this.sass_dir     = data.sass_dir
+
+    this.import_paths = data.import_paths.map(this.buildPath, this)
+    this.css_dir      = this.buildPath(data.css_dir)
+    this.sass_dir     = this.buildPath(data.sass_dir)
 }
 
 Options.prototype = {
 
+    buildPath: function(filePath)
+    {
+        return path.isAbsolute(filePath) ?
+            path.normalize(filePath) :
+            path.join(this.configDirname, filePath)
+    },
+
+    normalizeConfigPath: function(configPath)
+    {
+        return path.isAbsolute(configPath) ?
+            path.normalize(configPath) :
+            path.join(process.cwd(), configPath)
+    }
 }
 
 module.exports = Options
