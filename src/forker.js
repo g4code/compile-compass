df16ccb05a9a24a973ed4d85f58f2187ce763ff6
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-08-31 15:35:44 +0200

    Limit forks on 90% number of cups

--- src/forker.js
+++ src/forker.js
@@ -2,20 +2,29 @@ const Compiler      = require('./compiler')
 const File          = require('./file')
 const fork          = require('child_process').fork
 const evento        = require('evento')
+const os            = require('os')
 
 var Forker = function(options, files) {
 
-    this.options    = options
-    this.files      = files
-    this.filesCount = files.length
+    var noCups = os.cpus().length
+
+    this.options                = options
+    this.files                  = files
+    this.maxNumberOfProcesses   = noCups - Math.ceil(noCups * 0.1)
+    this.firstRoundFiles        = []
+    this.restOfFiles            = this.files.slice(0)
+
+    evento.trigger('INFORMER|INFO', 'Number of files: ' + files.length)
 }
 
 Forker.prototype = {
 
     fork: function()
     {
-        for (var i = 0; i < this.filesCount; i++) {
-            this.compile(this.files[i])
+        this.firstRoundFiles = this.restOfFiles.splice(0, this.maxNumberOfProcesses)
+
+        for (var i = 0; i < this.maxNumberOfProcesses; i++) {
+            this.compile(this.firstRoundFiles[i])
         }
     },
 
@@ -28,8 +37,9 @@ Forker.prototype = {
             evento.trigger(message.code, message.message)
         });
 
+        var self = this //TODO: Drasko - change this!
         workerProcess.on('close', function(code) {
-
+            self.restOfFiles.length > 0 && self.next()
         });
 
         workerProcess.send({
@@ -37,6 +47,12 @@ Forker.prototype = {
             oneFile: oneFile
         })
     },
+
+    next: function()
+    {
+        var nextFile = this.restOfFiles.splice(0, 1)[0]
+        this.compile(nextFile)
+    }
 
 }
 
